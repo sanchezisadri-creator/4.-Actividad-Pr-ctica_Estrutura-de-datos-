ed57c060106f9299dba2e79c2bcd9fbb34986450
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-03-11 12:20:50 -0400

    Subiendo el 9

---
+++ Ejercicio_9/Ejercicio_9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+       
+        Dictionary<string, int> numeros = new Dictionary<string, int>()
+        {
+            {"A", 10},
+            {"B", 20},
+            {"C", 30},
+            {"D", 40}
+        };
+
+        int suma = 0;
+
+        
+        foreach (var valor in numeros.Values)
+        {
+            suma += valor;
+        }
+
+        
+        Console.WriteLine("La suma de los valores es: " + suma);
+
+        Console.ReadKey();
+    }
+}
